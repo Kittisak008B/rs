4023e48a250dea5a6403b684918434d89fb71aab
PHAM <157298910+Kittisak008B@users.noreply.github.com>
2025-01-19 04:52:09 +0700

    Create enum01.rs

---
+++ enum01.rs
@@ -0,0 +1,23 @@
+enum Message {
+    Quit,                      // No data
+    Move { x: i32, y: i32 },   // Struct-like variant
+    Write(String),             // Tuple variant
+    ChangeColor(u8, u8, u8),   // RGB color
+}
+
+fn process_message(msg: Message) {
+    match msg {
+        Message::Quit => println!("Quit message received"),
+        Message::Move { x, y } => println!("Move to position: ({}, {})", x, y),
+        Message::Write(text) => println!("Message: {}", text),
+        Message::ChangeColor(r, g, b) => println!("Color changed to RGB({}, {}, {})", r, g, b),
+    }
+}
+
+fn main() {
+    let msg1 = Message::Move { x: 10, y: 20 };
+    let msg2 = Message::Write(String::from("Hello Rust!"));
+    
+    process_message(msg1);
+    process_message(msg2);
+}
